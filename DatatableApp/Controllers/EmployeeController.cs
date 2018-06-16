ccea04eb41bc19a5770817b31a0639484c943ed7
bajay <bajay@C229>
2018-06-16 12:21:17 +0530

    Add project files.

---
+++ DatatableApp/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatatableApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatatableApp.Controllers
+{
+    [Produces("application/json")]
+    public class EmployeeController : Controller
+    {
+        EmployeeDataAccessLayer objemployee = new EmployeeDataAccessLayer();
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<Employee> Index() => objemployee.GetAllEmployees();
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create([FromBody] Employee employee) => objemployee.AddEmployee(employee);
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public Employee Details(int id) => objemployee.GetEmployeeData(id);
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit([FromBody]Employee employee) => objemployee.UpdateEmployee(employee);
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id) => objemployee.DeleteEmployee(id);
+    }
+}
---
+++ DatatableApp/Models/EmployeeDataAccessLayer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DatatableApp.Models
+{
+    public class EmployeeDataAccessLayer
+    {
+        string connectionString = @"Data Source=C229\SQLEXPRESS2014;Initial Catalog=AngularDb;User ID=sa;Password=admin123!@#";
+
+        //To View all employees details  
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            try
+            {
+                var lstemployee = new List<Employee>();
+
+                using (var con = new SqlConnection(connectionString))
+                {
+                    var cmd = new SqlCommand("spGetAllEmployees", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    con.Open();
+                    var rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        var employee = new Employee
+                        {
+                            ID = Convert.ToInt32(rdr["EmployeeID"]),
+                            Name = rdr["Name"].ToString(),
+                            Gender = rdr["Gender"].ToString(),
+                            Department = rdr["Department"].ToString(),
+                            City = rdr["City"].ToString()
+                        };
+
+                        lstemployee.Add(employee);
+                    }
+                    con.Close();
+                }
+                return lstemployee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Add new employee record   
+        public int AddEmployee(Employee employee)
+        {
+            try
+            {
+                using (var con = new SqlConnection(connectionString))
+                {
+                    var cmd = new SqlCommand("spAddEmployee", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                    cmd.Parameters.AddWithValue("@Department", employee.Department);
+                    cmd.Parameters.AddWithValue("@City", employee.City);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Update the records of a particluar employee  
+        public int UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                using (var con = new SqlConnection(connectionString))
+                {
+                    var cmd = new SqlCommand("spUpdateEmployee", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    cmd.Parameters.AddWithValue("@EmpId", employee.ID);
+                    cmd.Parameters.AddWithValue("@Name", employee.Name);
+                    cmd.Parameters.AddWithValue("@Gender", employee.Gender);
+                    cmd.Parameters.AddWithValue("@Department", employee.Department);
+                    cmd.Parameters.AddWithValue("@City", employee.City);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Get the details of a particular employee  
+        public Employee GetEmployeeData(int id)
+        {
+            try
+            {
+                var employee = new Employee();
+
+                using (var con = new SqlConnection(connectionString))
+                {
+                    string sqlQuery = "SELECT * FROM tblEmployee WHERE EmployeeID= " + id;
+                    var cmd = new SqlCommand(sqlQuery, con);
+
+                    con.Open();
+                    var rdr = cmd.ExecuteReader();
+
+                    while (rdr.Read())
+                    {
+                        employee.ID = Convert.ToInt32(rdr["EmployeeID"]);
+                        employee.Name = rdr["Name"].ToString();
+                        employee.Gender = rdr["Gender"].ToString();
+                        employee.Department = rdr["Department"].ToString();
+                        employee.City = rdr["City"].ToString();
+                    }
+                }
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Delete the record on a particular employee  
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                using (var con = new SqlConnection(connectionString))
+                {
+                    var cmd = new SqlCommand("spDeleteEmployee", con)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    cmd.Parameters.AddWithValue("@EmpId", id);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
